e4421cd69e33e16149458b1165a5e612cf57ce64
riya-ingale <riya.ingale14@gmail.com>
2021-04-24 01:03:22 +0530

    Add and Update Doctor Appointments and Lab Results Feature

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ import numpy as np
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.urandom(24)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/PatientCenteredDatabase'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///PatientCenteredDatabase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mnir@postgresql123@localhost/PatientCenteredDatabase'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///PatientCenteredDatabase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['USE_SESSION_FOR_NEXT'] = True
 
@@ -43,14 +43,14 @@ class Roles(db.Model):
 
 class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(50))
+    user_name = db.Column(db.String(500), unique=True)
+    password = db.Column(db.String(500))
 
-    name = db.Column(db.String(50))
+    name = db.Column(db.String(500))
     dob = db.Column(db.Date)
-    city = db.Column(db.String(50))
-    phone = db.Column(db.Integer)
-    email = db.Column(db.String(50))
+    city = db.Column(db.String(500))
+    phone = db.Column(db.String(500))
+    email = db.Column(db.String(500))
 
     r_id = db.Column(db.Integer, db.ForeignKey('Roles.r_id'))
 
@@ -141,7 +141,7 @@ def signup():
             r_id = 0
         elif role == 'Doctor':
             r_id = 1
-        elif role == 'lab':
+        elif role == 'Lab':
             r_id = 2
         else:
             r_id = 3
@@ -176,7 +176,12 @@ def login():
             if check_password_hash(user.password, password):
                 print("password is correct")
                 login_user(user)
-                return current_user.name
+                if current_user.r_id == 1:
+                    return redirect(f'/dashboard/doctor/{current_user.id}')
+                if current_user.r_id == 2:
+                    return redirect(f'/dashboard/lab/{current_user.id}')
+                if current_user.r_id == 3:
+                    return redirect(f'/dashboard/patient/{current_user.id}')
             else:
                 flash("Incorrect Password")
                 return redirect(url_for('login'))
@@ -195,148 +200,271 @@ def logout():
 
 # # DASHBOARD - PATIENT
 
-# @app.route('/dashboard/patient/<int:user_id>', methods=['GET'])
-# def patient_dash(user_id):
-#     user = PatientLab.query.filter_by(patient_id=user_id).all()
-#     return 'pass data in frontend'
+@app.route('/dashboard/patient/<int:user_id>', methods=['GET'])
+def patient_dash(user_id):
+    user_reports = PatientLab.query.filter_by(patient_id=user_id).all()
+    user_apps = DoctorAppointment.query.filter_by(patient_id=user_id).all()
+    df = pd.DataFrame(columns=['dates', 'hemoglobin',
+                               'platelets', 'rbc_count', 'pcv', 'mcv', 'mchc'])
+    for user in user_reports:
+        print(user.hemoglobin)
+        df = df.append({'dates': user.date_collected, 'hemoglobin': user.hemoglobin, 'platelets': user.platelets, 'rbc_count': user.rbc_count,
+                        'pcv': user.pcv, 'mcv': user.mcv, 'mchc': user.mchc}, ignore_index=True)
+
+    fig = figure(title='hemoglobin', plot_width=400,
+                 plot_height=300, x_axis_type="datetime", responsive=True)
+    df['dates'] = pd.to_datetime(df['dates'])
+    print(df['hemoglobin'])
+    source = ColumnDataSource(df)
+    fig.line(
+        x='dates',
+        y='hemoglobin',
+        source=source,
+        line_width=2,
+        line_color='#17252A'
+    )
+    fig.circle(x='dates', y='hemoglobin', source=source,
+               fill_color="#DEF2F1", size=8)
+    fig.add_tools(HoverTool(tooltips=[("Y", "@hemoglobin")]))
+    fig.border_fill_color = "#DEF2F1"
+    fig.min_border_right = 80
+    s2 = figure(title='platelets', plot_width=400, plot_height=300,
+                x_axis_type="datetime", responsive=True)
+    source = ColumnDataSource(df)
+    s2.line(
+        x='dates',
+        y='platelets',
+        source=source,
+        line_width=2,
+        line_color='#17252A'
+    )
+    s2.circle(x='dates', y='platelets', source=source,
+              fill_color="#DEF2F1", size=8)
+    s2.add_tools(HoverTool(tooltips=[("Y", "@platelets")]))
+    s2.border_fill_color = "#DEF2F1"
+    s2.min_border_right = 80
+    # create and another
+    s3 = figure(title='rbc_count', plot_width=400, plot_height=300,
+                x_axis_type="datetime", responsive=True)
+    source = ColumnDataSource(df)
+    s3.line(
+        x='dates',
+        y='rbc_count',
+        source=source,
+        line_width=2,
+        line_color='#17252A'
+    )
+    s3.circle(x='dates', y='rbc_count', source=source,
+              fill_color="#DEF2F1", size=8)
+    s3.add_tools(HoverTool(tooltips=[("Y", "@rbc_count")]))
+    s3.border_fill_color = "#DEF2F1"
+    s3.min_border_right = 80
+    s4 = figure(title='pcv', plot_width=400, plot_height=300,
+                x_axis_type="datetime", responsive=True)
+    source = ColumnDataSource(df)
+    s4.line(
+        x='dates',
+        y='pcv',
+        source=source,
+        line_width=2,
+        line_color='#17252A'
+    )
+    s4.circle(x='dates', y='pcv', source=source, fill_color="#DEF2F1", size=8)
+    s4.add_tools(HoverTool(tooltips=[("Y", "@pcv")]))
+    s4.border_fill_color = "#DEF2F1"
+    s4.min_border_right = 80
+    s5 = figure(title='mcv', plot_width=400, plot_height=300,
+                x_axis_type="datetime", responsive=True)
+    source = ColumnDataSource(df)
+    s5.line(
+        x='dates',
+        y='mcv',
+        source=source,
+        line_width=2,
+        line_color='#17252A'
+    )
+    s5.circle(x='dates', y='mchc', source=source, fill_color="#DEF2F1", size=8)
+    s5.add_tools(HoverTool(tooltips=[("Y", "@mcv")]))
+    s5.border_fill_color = "#DEF2F1"
+    s5.min_border_right = 80
+    s6 = figure(title='mchc', plot_width=400, plot_height=300,
+                x_axis_type="datetime", responsive=True)
+    source = ColumnDataSource(df)
+    s6.line(
+        x='dates',
+        y='mchc',
+        source=source,
+        line_width=2,
+        line_color='#17252A'
+    )
+    s6.circle(x='dates', y='x', source=source, fill_color="#DEF2F1", size=8)
+    s6.add_tools(HoverTool(tooltips=[("Y", "@mchc")]))
+    s6.border_fill_color = "#DEF2F1"
+    s6.min_border_right = 80
+
+    p = gridplot([[fig, s2], [s3, s4], [s5, s6]])
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    # render template
+    script, div = components(p)
+    html = render_template(
+        'patients.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+        user_apps=user_apps
+    )
+    return encode_utf8(html)
 
 # # DASHBOARD - LAB
 
 
-# @app.route('/dashboard/lab/<int:user_id>', methods=['GET', 'POST'])
-# def lab_dash(user_id):
-#     if request.method == 'GET':
-#         return 'dashboard asking patient no'
-#     else:
-#         patient_no = request.form['patient_no']
-#         patient_entries = PatientLab.query.filter_by(
-#             lab_id=user_id, patient_id=patient_no).all()
-#         return 'direct to the information of pateints taken by the lab keep the ability to update and add'
-#         'Basically show template'
+@app.route('/dashboard/lab/<int:user_id>', methods=['GET', 'POST'])
+def lab_dash(user_id):
+    if request.method == 'GET':
+        return 'dashboard asking patient no'
+    else:
+        patient_no = request.form['patient_no']
+        patient_entries = PatientLab.query.filter_by(
+            lab_id=user_id, patient_id=patient_no).all()
+        return 'direct to the information of pateints taken by the lab keep the ability to update and add'
+        'Basically show template'
 
 
-# @app.route('/lab/<int:user_id>/<int: patient_id>/add', methods=["GET", "POST"])
-# def lab_add(user_id, patient_id):
-#     if request.method == "GET":
-#         return 'form'
-#     else:
-#         # take data from and then commit
-#         date_collected = datetime.now()
-#         hemoglobin = request.form.get('hemoglobin')
-#         platelets = request.form.get('platelets')
-#         rbc_count = request.form.get('rbc_count')
-#         pcv = request.form.get('pcv')
-#         mcv = request.form.get('mcv')
-#         mchc = request.form.get('mchc')
-#         mch = request.form.get('mch')
-#         new_entry = PatientLab(patient_id=patient_id, lab_id=user_id, hemoglobin=hemoglobin,
-#                                platelets=platelets, rbc_count=rbc_count, pcv=pcv, mcv=mcv, mchc=mchc, mch=mch)
-#         db.session.add(new_entry)
-#         db.session.commit()
+@app.route('/lab/add/<int:user_id>/<int:patient_id>', methods=["GET", "POST"])
+def lab_add(user_id, patient_id):
+    if request.method == "GET":
+        return render_template('add_lab_results.html', current_user=current_user, pid=patient_id)
+    else:
+        # take data from and then commit
+        date_collected = request.form.get('date_collected')
+        date_collected_obj = datetime.strptime(date_collected, '%Y-%m-%d')
+        hemoglobin = request.form.get('hemoglobin')
+        platelets = request.form.get('platelets')
+        rbc_count = request.form.get('rbc_count')
+        pcv = request.form.get('pcv')
+        mcv = request.form.get('mcv')
+        mchc = request.form.get('mchc')
+        new_entry = PatientLab(patient_id=patient_id, lab_id=user_id, date_collected=date_collected_obj,
+                               hemoglobin=hemoglobin, platelets=platelets, rbc_count=rbc_count, pcv=pcv, mcv=mcv, mchc=mchc)
+        db.session.add(new_entry)
+        db.session.commit()
 
-#         # retrive data from db
-#         return 'show template'
+        # retrive data from db
+        return 'show template'
 
 
-# @app.route('/lab/<int:user_id>/<int: patient_id>/update/<int: report_id>', methods=["GET", "POST"])
-# def lab_edit(user_id, patient_id, report_id):
-#     patient = PatientLab.query.filter_by(
-#         lab_id=user_id, patient_id=patient_no, report_id=report_id).first()
+@app.route('/lab/update/<int:user_id>/<int:patient_id>/<int:report_id>', methods=["GET", "POST"])
+def lab_edit(user_id, patient_id, report_id):
+    patient = PatientLab.query.filter_by(report_id=report_id).first()
+    if request.method == 'GET':
+        return render_template('update_lab_results.html', patient=patient)
+    else:
+        # take data from form and then edit in db
+        patient.date_collected = datetime.strptime(
+            request.form.get('date_collected'), '%Y-%m-%d')
+        patient.hemoglobin = request.form.get('hemoglobin')
+        patient.platelets = request.form.get('platelets')
+        patient.rbc_count = request.form.get('rbc_count')
+        patient.pcv = request.form.get('pcv')
+        patient.mcv = request.form.get('mcv')
+        patient.mchc = request.form.get('mchc')
+        db.session.commit()
 
-#     if request.method == 'GET':
-#         return 'edit form along with patient'
-#     else:
-#         # take data from form and then edit in db
-#         patient.date_collected = datetime.now()
-#         patient.hemoglobin = request.form.get('hemoglobin')
-#         patient.platelets = request.form.get('platelets')
-#         patient.rbc_count = request.form.get('rbc_count')
-#         patient.pcv = request.form.get('pcv')
-#         patient.mcv = request.form.get('mcv')
-#         patient.mchc = request.form.get('mchc')
-#         patient.mch = request.form.get('mch')
-#         db.session.commit()
-
-#         # retrive data from db
-#         return 'show template'
+        # retrive data from db
+        return 'show template'
 
 # # DASHBOARD - DOCTOR
 
 
-# @app.route('/dashboard/doctor/<int:user_id>', methods=['GET', 'POST'])
-# def doctor_dash(user_id):
-#     if g.user:
-#         if request.method == "GET":
-#             doc_appointments = DoctorAppointment.query.filter_by(
-#                 doc_id=user_id).all()
-#             'pass every thing to frontend but ask them to only show p_id , next_app , surger_dates , HOspital'
-#             return 'dashboard template showing this info and asking for patient no'
-#         else:
-#             patient_no = request.form['patient_no']
-#             patient_lab_results = patientLab.query.filter_by(
-#                 patient_id=patient_no).all()
-#             patient_prev_appointments = DoctorAppointment.query.filter_by(
-#                 patient_id=patient_no).all()
-#             return 'show these information in show template and show edit option to only of his appointment'
+@app.route('/dashboard/doctor/<int:user_id>', methods=['GET', 'POST'])
+def doctor_dash(user_id):
+    if request.method == "GET":
+        doc_appointments = DoctorAppointment.query.filter_by(
+            doc_id=user_id).all()
+        'pass every thing to frontend but ask them to only show p_id , next_app , surger_dates , HOspital'
+        return render_template('doctor.html', doc_appointments=doc_appointments, current_user=current_user)
+    else:
+        patient_no = request.form['patient_no']
+        patient_lab_results = PatientLab.query.filter_by(
+            patient_id=patient_no).all()
+        patient_prev_appointments = DoctorAppointment.query.filter_by(
+            patient_id=patient_no).all()
+        return 'show these information in show template and show edit option to only of his appointment'
 
 
-# @app.route('/doctor/<int:user_id>/<int: patient_id>/add', methods=["GET", "POST"])
-# def doctor_add(user_id, patient_id):
-#     if request.method == "GET":
-#         return 'form'
-#     else:
-#         # take data from and then commit
-#         hospital = request.form.get('hospital')
-#         appointment_date = request.form.get('appointment_date')
-#         symptoms = request.form.get('symptoms')
-#         medicines = request.form.get('medicines')
-#         next_appointment_date = request.form.get('next_appointment_date')
-#         recommended_lab_tests = request.form.get('recommended_lab_tests')
+@app.route('/doctor/add/<int:user_id>/<int:patient_id>', methods=["GET", "POST"])
+def doctor_add(user_id, patient_id):
+    if request.method == "GET":
+        return render_template('add_appointment_details.html', pid=patient_id)
+    else:
+        # take data from and then commit
+        hospital = request.form.get('hospital')
+        appointment_date = request.form.get('appointment_date')
+        appointment_date_obj = datetime.strptime(appointment_date, '%Y-%m-%d')
+        symptoms = request.form.get('symptoms')
+        medicines = request.form.get('medicines')
+        next_appointment_date = request.form.get('next_appointment_date')
+        next_appointment_date_obj = datetime.strptime(
+            next_appointment_date, '%Y-%m-%d')
+        recommended_lab_tests = request.form.get('recommended_lab_tests')
 
-#         allergy = request.form.get('allergy')
-#         surgery = request.form.get('surgery')
-#         surgery_date = request.form.get('surgery_date')
+        allergy = request.form.get('allergy')
+        surgery = request.form.get('surgery')
+        surgery_date = request.form.get('surgery_date')
+        if surgery_date != "":
+            surgery_date_obj = datetime.strptime(surgery_date, '%Y-%m-%d')
+        else:
+            surgery_date_obj = None
 
-#         new_entry = DoctorAppointment(patient_id=patient_id, doc_id=user_id, hospital=hospital, appointment_date=appointment_date, symptoms=symptoms, medicines=medicines,
-#                                       next_appointment_date=next_appointment_date, recommended_lab_tests=recommended_lab_tests, allergy=allergy, surgery=surgery, surgery_date=surgery_date)
-#         db.session.add(new_entry)
-#         db.session.commit()
+        new_entry = DoctorAppointment(patient_id=patient_id, doc_id=user_id, hospital=hospital, appointment_date=appointment_date_obj, symptoms=symptoms, medicines=medicines,
+                                      next_appointment_date=next_appointment_date_obj, recommended_lab_tests=recommended_lab_tests, allergy=allergy, surgery=surgery, surgery_date=surgery_date_obj)
+        db.session.add(new_entry)
+        db.session.commit()
 
-#         patient_lab_results = patientLab.query.filter_by(
-#             patient_id=patient_id).all()
-#         patient_prev_appointments = DoctorAppointment.query.filter_by(
-#             patient_id=patient_id).all()
-#         return 'show these information in show template and show edit option to only of his appointment'
+        patient_lab_results = PatientLab.query.filter_by(
+            patient_id=patient_id).all()
+        patient_prev_appointments = DoctorAppointment.query.filter_by(
+            patient_id=patient_id).all()
+        return 'show these information in show template and show edit option to only of his appointment'
 
 
-# @app.route('/doctor/<int:user_id>/<int: patient_id>/update/<int: app_id>', methods=["GET", "POST"])
-# # user_id==doctor_id
-# def doc_edit(user_id, patient_id, app_id):
-#     patient = DoctorAppointment.query.filter_by(
-#         doc_id=user_id, patient_id=patient_id, app_id=app_id).first()
+@app.route('/doctor/update/<int:user_id>/<int:patient_id>/<int:app_id>', methods=["GET", "POST"])
+# user_id==doctor_id
+def doc_edit(user_id, patient_id, app_id):
+    patient = DoctorAppointment.query.filter_by(app_id=app_id).first()
 
-#     if request.method == 'GET':
-#         return 'edit form along with patient'
-#     else:
-#         # take data from form and then edit in db
-#         patient.hospital = request.form.get('hospital')
-#         patient.appointment_date = request.form.get('appointment_date')
-#         patient.symptoms = request.form.get('symptoms')
-#         patient.medicines = request.form.get('medicines')
-#         patient.next_appointment_date = request.form.get(
-#             'next_appointment_date')
-#         patient.recommended_lab_tests = request.form.get(
-#             'recommended_lab_tests')
-#         patient.allergy = request.form.get('allergy')
-#         patient.surgery = request.form.get('surgery')
-#         patient.surgery_date = request.form.get('surgery_date')
-#         db.session.commit()
+    if request.method == 'GET':
+        return render_template('update_appointment_details.html', patient=patient)
+    else:
+        # take data from form and then edit in db
+        patient.hospital = request.form.get('hospital')
+        patient.appointment_date = request.form.get('appointment_date')
+        patient.symptoms = request.form.get('symptoms')
+        patient.medicines = request.form.get('medicines')
+        if request.form.get('next_appointment_date') != "":
+            patient.next_appointment_date = datetime.strptime(
+                request.form.get('next_appointment_date'), '%Y-%m-%d')
+        else:
+            patient.next_appointment_date = None
+        patient.recommended_lab_tests = request.form.get(
+            'recommended_lab_tests')
+        patient.allergy = request.form.get('allergy')
+        patient.surgery = request.form.get('surgery')
+        if request.form.get('surgery_date') != "":
+            patient.surgery_date = datetime.strptime(
+                request.form.get('surgery_date'), '%Y-%m-%d')
+        else:
+            patient.surgery_date = None
+        db.session.commit()
 
-#         # retrive data from db
-#         appointments = DoctorAppointment.query.filter_by(
-#             doc_id=user_id, patient_id=patient_id).all()
+        # retrive data from db
+        appointments = DoctorAppointment.query.filter_by(
+            doc_id=user_id, patient_id=patient_id).all()
 
-#         return 'show template with all appointments'
+        return 'show template with all appointments'
 
 
 @app.route('/lab', methods=["GET", "POST"])
